5116d58cce541605749912ecb1d9342b428a8d66
Gerson Rocha <gersonrocha9@gmail.com>
2022-02-24 16:08:32 -0300

    content component done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,1 +1,21 @@
-export function SideBar() {}
+export function SideBar() {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
